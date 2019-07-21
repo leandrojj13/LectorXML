86a0ad6679abe08b3296695883ee7051f695111b
Leandro <leandrojj13@hotmail.com>
2019-07-21 00:54:33 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Recibidor.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace Recibidor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetOrdenes()
+        {
+            var receivementList = new List<Receivement>();
+
+            foreach (string file in Directory.EnumerateFiles(@"C:\Temp\XMLS", "*.xml"))
+            {
+                string contents = System.IO.File.ReadAllText(file);
+
+                var stringReader = new StringReader(contents);
+
+                XmlRootAttribute xRoot = new XmlRootAttribute
+                {
+                    ElementName = "Receivement",
+                    IsNullable = true
+                };
+
+                var serializer = new XmlSerializer(typeof(Receivement), xRoot);
+
+                var receivement = serializer.Deserialize(stringReader) as Receivement;
+
+                receivementList.Add(receivement);
+            }
+
+            return Json(receivementList, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/Orden.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Recibidor.Models
+{
+    [XmlRoot(ElementName = "Receivement")]
+    public class Receivement
+    {
+        //public Guid NoOrden { get; set; }
+        public string Employee { get; set; }
+        public string Status { get; set; }
+        public string OrderNumber { get; set; }
+        public string Date { get; set; }
+        public string Total { get; set; }
+        public string CustomerEmailName { get; set; }
+        
+    }
+}
